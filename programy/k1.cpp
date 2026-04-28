4a90c068b8a292572ab0afd688816478cfc5cb5a
Szymon Urban <szymonur88828@gmail.com>
2026-04-28 14:45:37 +0200

    add k3a, modify k1,k2,k3

--- programy/k1.cpp
+++ programy/k1.cpp
@@ -4,22 +4,21 @@
 #include <cstdio>
 #include <omp.h>
 
-int main(){
-	int m = 0;
-	int n = 1e8;
-	double start_time, stop_time;
-
+int calculate(int n, int m){
 	bool* result = (bool*)malloc((n - m + 1) * sizeof(bool));
 	memset(result, true, (n - m + 1) * sizeof(bool));
+	int prime_counter = 0;
 
 	bool* primeArray = (bool*)malloc((sqrt(n) + 1) * sizeof(bool));
 	memset(primeArray, true, (sqrt(n) + 1) * sizeof(bool));
-
-	start_time = omp_get_wtime();
+	
+	if (m <= 0 && n >= 0) result[0 - m] = false;
+    if (m <= 1 && n >= 1) result[1 - m] = false;
 
 	for (int i = 2; i * i <= n; i++) {
 		for (int j = 2; j * j <= i; j++) {
-			if (primeArray[j] == true && i % j == 0) { primeArray[i] = false; 
+			if (primeArray[j] == true && i % j == 0) { 
+				primeArray[i] = false; 
 				break;
 			}
 		}
@@ -29,11 +28,29 @@ int main(){
 			if (primeArray[j] == true && i % j == 0) {
 				result[i - m] = false;
 				break; 
-			}
+			} 
+		}
+		if(result[i-m] == true){
+			prime_counter++;
 		}
 	}
+	free(result);
+    free(primeArray);
+
+	return prime_counter;
+}
+
+int main(){
+	int m = 0;
+	int n = 1e8;
+	double start_time, stop_time;
+
+	start_time = omp_get_wtime();
+	int prime_counter = 0;
+	prime_counter = calculate(n, m);
 	stop_time = omp_get_wtime();
 
+	printf("Liczba liczb pierwszych: %d\n", prime_counter);
 	printf("Czas trwania obliczen - wallclock %f sekund \n", stop_time-start_time);
 
 
--- programy/k3.cpp
+++ programy/k3.cpp
@@ -4,18 +4,14 @@
 #include <cstdio>
 #include <omp.h>
 
-int main(){
-	int m = 0;
-	int n = 1e8;
-	double start_time, stop_time;
-
+int calculate(int n, int m){
+	int prime_counter=0;
 	bool* result = (bool*)malloc((n - m + 1) * sizeof(bool));
 	memset(result, true, (n - m + 1) * sizeof(bool));
-
+		
 	bool* primeArray = (bool*)malloc((sqrt(n) + 1) * sizeof(bool));
 	memset(primeArray, true, (sqrt(n) + 1) * sizeof(bool));
-
-	start_time = omp_get_wtime();
+	
 	for (int i = 2; i*i*i*i <= n; i++) {
 		if (primeArray[i] == true) {
 			for (int j = i * i; j*j <= n; j+=i) {
@@ -30,13 +26,37 @@ int main(){
 				firstMultiple = i + i;
 			} else if (m % i) { 
 				firstMultiple = (firstMultiple * i) + i;
-			} else {firstMultiple = (firstMultiple * i);}
+			} else {
+				firstMultiple = (firstMultiple * i);
+			}
 			for (int j = firstMultiple; j <= n; j+=i) { 
 				result[j-m] = false;
 			}
 		}
 	}
+	if (m <= 0 && n >= 0) result[0 - m] = false;
+    if (m <= 1 && n >= 1) result[1 - m] = false;
+
+	for (int i = m; i <= n; i++) {
+    	if (result[i - m]) prime_counter++;
+	}
+
+	free(result);
+    free(primeArray);
+	
+	return prime_counter;
+}
+
+int main(){
+	int m = 0;
+	int n = 1e8;
+	double start_time, stop_time;
+
+	start_time = omp_get_wtime();
+	int prime_counter = 0;
+	prime_counter = calculate(n, m);
 	stop_time = omp_get_wtime();
 
+	printf("Liczba liczb pierwszych: %d\n", prime_counter);
 	printf("Czas trwania obliczen - wallclock %f sekund \n", stop_time-start_time);
 }
